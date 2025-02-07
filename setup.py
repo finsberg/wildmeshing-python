a50f93e90cbd00db5855a4588405592631aafb47
Henrik Finsberg <henrikfinsberg@hotmail.com>
2025-02-07 13:21:50 +0100

    Attempt to use cibuildhweel

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ setup(
         "Programming Language :: Python :: 3",
         'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     install_requires=[
         'svgwrite',
         'numpy',
