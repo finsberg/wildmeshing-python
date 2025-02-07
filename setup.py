b3c669f33a4e44dded4f0bfc5bf5386055d9a85a
Henrik Finsberg <henrikfinsberg@hotmail.com>
2025-02-07 09:30:59 +0100

    Update licese specifier

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MPL-2.0 License"
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
     ],
     python_requires='>=3.6',
     install_requires=[
